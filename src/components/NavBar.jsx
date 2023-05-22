3aa093e95fa0ee5bd23b92fbaceade746e0b8f5f
nicolamberto <nicolaslamberto2840@gmail.com>
2023-05-22 19:08:53 -0300

    removing contact component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -22,9 +22,7 @@ const navLinks = [
     {
         title: 'Projects', path: '#projects'
     },
-    {
-        title: 'Contact', path: '#contact'
-    },
+    
 ]
 
 export default function NavBar(){
